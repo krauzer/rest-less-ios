efdc896f555deb2e7a091f8a5a68b3cb88f40cc6
Dylan Krause <dykrause@gmail.com>
2014-08-17 22:48:59 -0400

    Update Testing To Align with API Protocol

--- Rest Less/SecondViewController.swift
+++ Rest Less/SecondViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
 
+class SecondViewController: UIViewController, APIGetWorkoutControllerProtocol {
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,6 +23,30 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func getWorkout(sender: AnyObject) {
+        
+        var params = ["exercise_history":["weight":10, "reps":11,"rest_time":90, "distance":15, "running_time":400, "workout_history_id":3, "exercise_id":3]] as Dictionary
+        HTTPostJSON("http://secret-stream-5880.herokuapp.com/exercise_histories",  params)
+        
+        var api = APIGetWorkoutController()
+        api.delegate = self
+        api.HTTPGetter("http://localhost:3000/workouts/1.json")
+        
+       
+    }
+    
+    var response:NSDictionary?
+    func receivedGetResponse(result: NSDictionary) -> NSDictionary {
+        
+        println(result)
+        response = result
+        println(response)
+        return response!
+    }
+    
+    
     
 
     /*
